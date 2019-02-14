eca31b52735fb8e0c27c7f8bda9f4ccc49a2684d
mio <mio093611@gmail.com>
2019-02-14 14:47:10 +0800

    Component 練習

---
+++ WebApplication1/WebApplication1.App/Services/Practice06_ChildComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApplication1.App.Services
+{
+    public class ChildComponent
+    {
+        public string ChildTitle { get; set; }
+
+        public string ChildContent { get; set; }
+    }
+}
